bb419256a5b3f62888b0550cf173b29a10c50cd6
partizanos <dimi.proios@gmail.com>
2016-04-26 01:58:02 +0200

    get request communication

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,31 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.shortcuts import render
 from django.http import HttpResponse
-
-import mongoengine
-# ...
+# import mongoengine
 from pymongo import MongoClient
-from django.template import Context, loader
-def index(request):
-	server="localhost"
-	port = 27017
-	c = MongoClient()
-	db=c.yelp
-	collection = db['tip']
+from django.template import loader
 
-	template = loader.get_template("polls/generic.html")
-	return HttpResponse(template.render())
+
+def index(request):
+    # server = "localhost"
+    # port = 27017
+    # c = MongoClient()
+    # db = c.yelp
+    # collection = db['tip']
+
+    template = loader.get_template("polls/generic.html")
+    return HttpResponse(template.render())
+
 
 def db_req(request):
-	server="localhost"
-	port = 27017
-	c = MongoClient()
-	db=c.yelp
-	collection = db['tip']
-	return HttpResponse(str(collection.find_one()))
+    if request.method == 'GET':
+        c = MongoClient()
+        db = c.yelp
+        collection = db['ptip']
+        value = request.GET.get('value').encode('utf8')
+        # collection = db['tip']
+        # return HttpResponse(request.body)
+        return HttpResponse(value)
+        # return HttpResponse(str(collection.find_one()))
     # return HttpResponse("Hello, world. You're at the polls index.")
 
-#Establish a connection with mongo instance.
+# Establish a connection with mongo instance.
 # conn = Connection(server,port)
 # assert isinstance(user, mongoengine.django.auth.User)
